7d14d15d018c635d5bdcdf5e841b5a817d5915a4
Christy Yuen <yuenchr1sty@gmail.com>
2023-08-27 18:49:25 -0700

    Updated Logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-import bunny from './components/bunny';
+//import bunny from './components/bunny';
 import 'tailwindcss/tailwind.css';
 
 function App() {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from "react-scroll"; //, animateScroll as scroll 
+import logo from './assests/logo.png';
 
 const highlight = "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-bold";
+const logo_size = "w-10 h-10 inline-block";
 
 function NavBar() {
   return (
@@ -10,7 +12,7 @@ function NavBar() {
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center h-16 justify-center">
             <div className="flex absolute left-10">
-                <span className="text-white">Logo</span>
+                <span className="text-white"><img src={logo} alt="Logo" className={logo_size} /> </span>
             </div>
             <div className="hidden md:block">
                 <div className="flex items-center justify-center flex-grow ml-10 space-x-20">
